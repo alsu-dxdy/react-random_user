10e203ba028b0de420db1ed7daa735f9dad3bffe
Alsu Abdrakhmanova <dxdy10@mail.ru>
2020-09-11 22:53:35 +0500

    Updates

---
+++ precache-manifest.04156e13af299678067a5449e5c18b88.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "498180207324c3e922e21b8b96f09d99",
+    "url": "/react-random_user/index.html"
+  },
+  {
+    "revision": "b245dd9a0a686b60ddc2",
+    "url": "/react-random_user/static/css/main.edc5d3b9.chunk.css"
+  },
+  {
+    "revision": "981a90612489880caa96",
+    "url": "/react-random_user/static/js/2.16c842ee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-random_user/static/js/2.16c842ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b245dd9a0a686b60ddc2",
+    "url": "/react-random_user/static/js/main.f6d69701.chunk.js"
+  },
+  {
+    "revision": "b93ed4fecd5e61c54b78",
+    "url": "/react-random_user/static/js/runtime-main.8fcbbfd6.js"
+  },
+  {
+    "revision": "429c87524d1c2a67d40e245d3aef7551",
+    "url": "/react-random_user/static/media/like-active.429c8752.svg"
+  },
+  {
+    "revision": "ed9e50d5224dd8511042e16f2a4dba2f",
+    "url": "/react-random_user/static/media/like-inactive.ed9e50d5.svg"
+  }
+]);
